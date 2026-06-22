e60c5cd92398f54cec624f8a005b201c1b9e8b98
cheenum <cheenum@example.com>
2026-06-22 12:38:09 +0530

    feat: add volume measurement (litre, ml, gallon) for UC11

--- src/main/java/quantitymeasurement/VolumeUnit.java
+++ src/main/java/quantitymeasurement/VolumeUnit.java
@@ -1,49 +1,31 @@
 package quantitymeasurement;
 
-/**
- * Represents VolumeUnit.
- */
 public enum VolumeUnit implements IMeasurable {
     LITRE(1.0),
     MILLILITRE(0.001),
     GALLON(3.78541);
 
-    /**
-     * Property baseUnitConversionFactor.
-     */
     private final double baseUnitConversionFactor;
 
     VolumeUnit(double baseUnitConversionFactor) {
         this.baseUnitConversionFactor = baseUnitConversionFactor;
     }
 
-    /**
-     * Execution logic for getConversionFactor.
-     */
     @Override
     public double getConversionFactor() {
         return this.baseUnitConversionFactor;
     }
 
-    /**
-     * Execution logic for convertToBaseUnit.
-     */
     @Override
     public double convertToBaseUnit(double value) {
         return value * this.baseUnitConversionFactor;
     }
 
-    /**
-     * Execution logic for convertFromBaseUnit.
-     */
     @Override
     public double convertFromBaseUnit(double baseValue) {
         return baseValue / this.baseUnitConversionFactor;
     }
 
-    /**
-     * Execution logic for getUnitName.
-     */
     @Override
     public String getUnitName() {
         return this.name().toLowerCase();
